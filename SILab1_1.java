d512e86b90cfd140448b9f46fd601ad9ba0e9da8
Teo <teovrondeliev12@gmail.com>
2024-04-12 19:41:59 +0200

    init commit

--- SILab1_1.java
+++ SILab1_1.java
@@ -5,10 +5,11 @@ public class SILab1_1 {
     public static List<String> filterOddLengthStrings(List<String> list) {
         List<String> result = new ArrayList<>();
         for (String el : list) {
-            if (el.length() % 2 == 0) {
+            if (el.length() % 2 != 0) {
                 result.add(el);
             }
         }
+
         return result;
     }
 
